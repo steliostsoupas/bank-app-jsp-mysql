848a64b72550fc2a7a93b3f686e3b60d75b9518f
steliostsoupas <stelios-tsoupas@hotmail.com>
2023-09-07 17:09:57 +0300

    add Customer's dto classes

---
+++ src/main/java/gr/personal/bankapp/dto/CustomerReadOnlyDTO.java
@@ -0,0 +1,60 @@
+package gr.personal.bankapp.dto;
+
+public class CustomerReadOnlyDTO extends Base {
+    private String firstname;
+    private String lastname;
+    private String contactInfo;
+    private String dateOfBirth;
+    private String address;
+    private String ssn;
+
+    CustomerReadOnlyDTO() {}
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
---
+++ src/main/java/gr/personal/bankapp/dto/CustomerUpdateDTO.java
@@ -0,0 +1,69 @@
+package gr.personal.bankapp.dto;
+
+public class CustomerUpdateDTO extends Base {
+    private String firstname;
+    private String lastname;
+    private String contactInfo;
+    private String dateOfBirth;
+    private String address;
+    private String ssn;
+
+    public CustomerUpdateDTO() {}
+
+    public CustomerUpdateDTO(String firstname, String lastname, String contactInfo, String dateOfBirth, String address, String ssn) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.contactInfo = contactInfo;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.ssn = ssn;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
